f6260cc2c710d5f7ccceede4940041c94d311d95
nazir <nazir.alicanov@gmail.com>
2020-01-25 19:24:54 +0400

    first commit

---
+++ src/main/java/tech/nosy/nosypush/NosyPushApplication.java
@@ -0,0 +1,13 @@
+package tech.nosy.nosypush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosyPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosyPushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/nosy/nosypush/NosyPushApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nosy.nosypush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosyPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
